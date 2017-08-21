15550a4c0f7b1c81f5efc04cb7659724dada8af3
happystick <sai.sasank.yadati@gmail.com>
2017-08-21 21:36:50 +0530

    Initial changes

--- Unit-1/Adversarial Search/environments.py
+++ Unit-1/Adversarial Search/environments.py
@@ -11,10 +11,61 @@ class TicTacToe:
 
     The initial state of the environment is an empty 3x3 board.
 
-    Possible actions in this environment : 1,2,...9 ( positions on the board ).
+    Possible actions in this environment : 1,2,...,8,9 ( positions on the board ).
 
-    Every state is associated with some utility for a given player.
+    Every terminal state is associated with some utility for both players.
 
-    Every state has a reference to it's parent (state from which it
+    Every state has a reference to its parent (state from which it
     has been resulted from).
+
+    This is an adversarial environment. 'X' always goes first.
+    ->Fully observable
+    ->Deterministic
+    ->Discrete
+    ->Static
+    ->Sequential
     '''
+
+    def __init__(self):
+        self.state = [' ' for x in range(0,9)]
+
+        pass
+
+    def actions(self):
+        '''
+        recieves an object of type environment, which describes
+        a state of the environment.
+
+        returns a set of actions that can be taken in this state.
+
+        the set of actions that can be taken depends on the position
+        of the X's and O's on the board.
+        '''
+
+        actions = list()
+
+        return actions
+
+    def player(self):
+        '''
+        recieves an object of type environment, which describes
+        a state of the environment.
+
+        returns 'X' or 'O' by determining the player who holds the turn.
+        '''
+
+        pass
+
+    def transistion(self,action):
+        '''
+
+        '''
+        pass
+
+    def terminal_test(self):
+        '''
+
+        '''
+        pass
+
+    def
